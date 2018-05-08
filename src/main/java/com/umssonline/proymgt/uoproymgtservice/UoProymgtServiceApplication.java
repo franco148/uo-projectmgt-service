c81f852af407a0336398fcfb69309f60ec43ec92
franco-extreme <franck_ral@hotmail.com>
2018-05-07 20:51:19 -0400

    uo-projectmgt-service: Initial structure of the project.

---
+++ src/main/java/com/umssonline/proymgt/uoproymgtservice/UoProymgtServiceApplication.java
@@ -0,0 +1,12 @@
+package com.umssonline.proymgt.uoproymgtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UoProymgtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UoProymgtServiceApplication.class, args);
+	}
+}
