0e7c97dc42f7cdc3d3bcf4881250cf63138431ec
franco-extreme <franck_ral@hotmail.com>
2018-05-24 00:32:02 -0400

    ProjMgtService-SprintFeature: Implemented the Sprint controller.

--- src/main/java/com/umssonline/proymgt/controllers/SprintRestController.java
+++ src/main/java/com/umssonline/proymgt/controllers/SprintRestController.java
@@ -1,13 +1,11 @@
 package com.umssonline.proymgt.controllers;
 
+import com.umssonline.proymgt.controllers.request.SprintDto;
 import com.umssonline.proymgt.models.Sprint;
 import com.umssonline.proymgt.services.SprintService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -30,6 +28,38 @@ public class SprintRestController {
             return ResponseEntity.ok(sprintFromDb);
         } catch (Exception ex) {
             String errorMessage = "Sprint can not be found: " + ex.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody Sprint sprint) {
+        Sprint savedSprint = sprintService.save(sprint);
+
+        return ResponseEntity.ok(savedSprint);
+    }
+
+    @PatchMapping
+    public ResponseEntity update(@RequestBody SprintDto editedSprint) {
+
+        try {
+            Sprint sprintFromDb = sprintService.edit(editedSprint);
+
+            return ResponseEntity.ok(sprintFromDb);
+        } catch (Exception ex) {
+            String errorMessage = "Sprint can not be found: " + ex.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+
+        try {
+            sprintService.delete(id);
+            return ResponseEntity.ok().body("Sprint was deleted successfully");
+        } catch (Exception ex) {
+            String errorMessage = "Sprint can not be deleted because: " + ex.getMessage();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
         }
     }
