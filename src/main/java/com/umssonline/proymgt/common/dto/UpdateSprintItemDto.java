0f4d12864ebc5c9ecd8eafbfcc50926fb3650147
franco-extreme <franck_ral@hotmail.com>
2018-05-27 22:58:25 -0400

    ProjMgtService-TaskFeature: Creating the controller for Task feature.

--- src/main/java/com/umssonline/proymgt/common/dto/UpdateSprintItemDto.java
+++ src/main/java/com/umssonline/proymgt/common/dto/UpdateSprintItemDto.java
@@ -5,22 +5,23 @@ import com.umssonline.proymgt.models.Priority;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class CreateUserStoryDto {
+public class UpdateSprintItemDto {
 
+    @Id
+    @NotNull
+    private Long id;
     @NotNull
     private String name;
     private String description;
     private Priority priority;
     private Integer estimatedTime;
 
-    @JsonIgnore
-    private LocalDate createdOn = LocalDate.now();
     @JsonIgnore
     private LocalDateTime updatedOn = LocalDateTime.now();
 }
