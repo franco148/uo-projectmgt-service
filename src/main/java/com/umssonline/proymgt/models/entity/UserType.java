57486551ebf5e14936c0d15615471d45ea78dde8
franco-extreme <franck_ral@hotmail.com>
2018-08-07 18:57:50 -0400

    ProjMgtService-ImplementControllers: Updated UserType enum since it was updated in AuthServer.

--- src/main/java/com/umssonline/proymgt/models/entity/UserType.java
+++ src/main/java/com/umssonline/proymgt/models/entity/UserType.java
@@ -1,11 +1,10 @@
 package com.umssonline.proymgt.models.entity;
 
 public enum UserType {
-    PRODUCT_OWNER,
-    SCRUM_MASTER,
-    DEVELOPER,
-    MANAGER,
-    QA,
-    DEVOPS,
-    IT
+    ADMIN,
+    TRIBUNAL,
+    DIRECTOR,
+    PROFESSOR,
+    TUTOR,
+    STUDENT
 }
