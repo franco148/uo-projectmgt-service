a2a445b5c58cd92b96b803cc34ff7ca5578898ce
franco-extreme <franck_ral@hotmail.com>
2018-08-07 22:25:16 -0400

    ProjMgtService-ImplementControllers: Added swagger documentation to the ProjectRestController class.

--- src/main/java/com/umssonline/proymgt/controllers/ProjectRestController.java
+++ src/main/java/com/umssonline/proymgt/controllers/ProjectRestController.java
@@ -31,17 +31,136 @@ public interface ProjectRestController {
             )
             final CreateProjectDto project);
 
+
+    @ApiOperation
+    (
+        notes = "Find all projects of the system.",
+        value = "Find all projects",
+        nickname = "findAll",
+        code = 302
+    )
     ResponseEntity<Iterable<Project>> findAll();
 
-    ResponseEntity<Project> findById(final Long projectId);
 
-    ResponseEntity<Project> update(final Long projectId, final UpdateProjectDto project);
+    @ApiOperation
+    (
+        notes = "Find a project with a specified ID",
+        value = "Find Project by ID",
+        nickname = "findById",
+        code = 302
+    )
+    ResponseEntity<Project> findById(
+            @ApiParam
+            (
+                value = "ProjectId that needs to be fetched.",
+                type = "Number",
+                required = true
+            )
+            final Long projectId);
 
-    ResponseEntity<Void> delete(final Long projectId);
 
-    ResponseEntity<Sprint> addSprint(final Long projectId, final CreateSprintDto sprint);
+    @ApiOperation
+    (
+            notes = "Update a Project with a specified ID",
+            value = "Update Project by ID",
+            nickname = "update",
+            code = 200
+    )
+    ResponseEntity<Project> update(
+            @ApiParam
+            (
+                value = "ProjectId which is going to be updated.",
+                type = "Number",
+                required = true
+            )
+            final Long projectId,
+            @ApiParam
+            (
+                value = "Project object which contains updated data.",
+                type = "UpdateProjectDto",
+                required = true
+            )
+            final UpdateProjectDto project);
 
-    ResponseEntity<Project> loadSprintsFromProject(final Long projectId);
 
-    ResponseEntity<Boolean> entityExist(final String entityType, final Serializable entityId);
+    @ApiOperation
+    (
+        notes = "Delete a project with a specified ID",
+        value = "Delete Project by ID",
+        nickname = "delete",
+        code = 204
+    )
+    ResponseEntity<Void> delete(
+            @ApiParam
+            (
+                value = "ProjectId which is going to be deleted.",
+                type = "Number",
+                required = true
+            )
+            final Long projectId);
+
+
+    @ApiOperation
+    (
+        notes = "Create a new Sprint in project with a specified ID",
+        value = "Add new sprint to project",
+        nickname = "addSprint",
+        code = 201
+    )
+    ResponseEntity<Sprint> addSprint(
+            @ApiParam
+            (
+                value = "ProjectId in which a new sprint is going to be created.",
+                type = "Number",
+                required = true
+            )
+            final Long projectId,
+            @ApiParam
+            (
+                value = "ProjectId which is going to be deleted.",
+                type = "CreateSprintDto",
+                required = true
+            )
+            final CreateSprintDto sprint);
+
+
+    @ApiOperation
+    (
+        notes = "Get Sprints from a project with a specified ID",
+        value = "Get Sprints from Project",
+        nickname = "loadSprintsFromProject",
+        code = 302
+    )
+    ResponseEntity<Project> loadSprintsFromProject(
+            @ApiParam
+            (
+                value = "ProjectId from which sprints will going to be load.",
+                type = "Number",
+                required = true
+            )
+            final Long projectId);
+
+
+    @ApiOperation
+    (
+        notes = "Verify if a entity with a specified ID exists.",
+        value = "Does entity exist",
+        nickname = "entityExist",
+        code = 302
+    )
+    ResponseEntity<Boolean> entityExist(
+            @ApiParam
+            (
+                value = "Entity type that needs to be verified.",
+                type = "String",
+                required = true
+            )
+            final String entityType,
+            @ApiParam
+            (
+                value = "Entity ID that needs to be verified.",
+                type = "Number",
+                required = true
+            )
+            final Long entityId);
 }
