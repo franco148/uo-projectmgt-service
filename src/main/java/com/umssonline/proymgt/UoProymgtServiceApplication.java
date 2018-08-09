8bec67d7423886a70c91cd7f5979ac1571f3a54d
franco-extreme <franck_ral@hotmail.com>
2018-08-08 23:38:20 -0400

    ProjMgtService-ImplementProjectProcess: Added required dependency for FeignClient and Enabled it into the project.

--- src/main/java/com/umssonline/proymgt/UoProymgtServiceApplication.java
+++ src/main/java/com/umssonline/proymgt/UoProymgtServiceApplication.java
@@ -2,9 +2,13 @@ package com.umssonline.proymgt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableEurekaClient
+@EnableFeignClients(basePackages = {"com.umssonline.proymgt.feign"})
+@EnableCircuitBreaker
 @SpringBootApplication
 public class UoProymgtServiceApplication {
 
