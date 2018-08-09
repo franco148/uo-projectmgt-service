c94dc7a062a9b26d9f830388370ccd1235db845f
franco-extreme <franck_ral@hotmail.com>
2018-08-08 23:39:44 -0400

    ProjMgtService-ImplementProjectProcess: Implemented FeignClient for Users.

--- src/main/java/com/umssonline/proymgt/feign/UsersFeignClient.java
+++ src/main/java/com/umssonline/proymgt/feign/UsersFeignClient.java
@@ -1,4 +1,22 @@
 package com.umssonline.proymgt.feign;
 
+import com.umssonline.proymgt.models.entity.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Component
+@FeignClient("AUTH-SERVICE")
 public interface UsersFeignClient {
+
+    @RequestMapping
+    (
+        value = "/users/{user_id}",
+        method = RequestMethod.GET,
+        consumes = { "application/json" },
+        produces = { "application/json" }
+    )
+    User findById(@PathVariable("user_id") final Long userId);
 }
