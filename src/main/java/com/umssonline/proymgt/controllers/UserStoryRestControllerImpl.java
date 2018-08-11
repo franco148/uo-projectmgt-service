ac00a7e8006ab7c70eaf3386a0a0830b8bb33588
franco-extreme <franck_ral@hotmail.com>
2018-08-11 01:18:57 -0400

    ProjMgtService-ImplementBacklogProcess: Defined methods for UserStoryRestController.

--- src/main/java/com/umssonline/proymgt/controllers/UserStoryRestControllerImpl.java
+++ src/main/java/com/umssonline/proymgt/controllers/UserStoryRestControllerImpl.java
@@ -1,6 +1,8 @@
 package com.umssonline.proymgt.controllers;
 
 import com.umssonline.proymgt.models.dto.story.UpdateUserStoryDto;
+import com.umssonline.proymgt.models.dto.task.CreateTaskDto;
+import com.umssonline.proymgt.models.entity.Sprint;
 import com.umssonline.proymgt.models.entity.Task;
 import com.umssonline.proymgt.models.entity.UserStory;
 import com.umssonline.proymgt.services.api.UserStoryService;
@@ -10,10 +12,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("/user-stories")
-public class UserStoryRestControllerImpl {
+public class UserStoryRestControllerImpl implements UserStoryRestController {
 
     //region Properties
     @Autowired
@@ -25,41 +29,75 @@ public class UserStoryRestControllerImpl {
 
     //region Methods
     //Need to build queries by everything
-    public ResponseEntity<UserStory> findAllBy() {
+//    public ResponseEntity<UserStory> findAllBy() {
+//        return null;
+//    }
+//
+//    @GetMapping("/{us_id}")
+//    public ResponseEntity findById(@PathVariable("us_id") final Long id) {
+//
+//        UserStory userStory = userStoryService.findById(id);
+//
+//        return ResponseEntity.ok(userStory);
+//    }
+//
+//    @PutMapping("/{us_id}")
+//    public ResponseEntity<UserStory> update(@PathVariable("us_id") final Long id, @RequestBody final UpdateUserStoryDto userStory) {
+//
+//        UserStory converted = modelMapper.map(userStory, UserStory.class);
+//        converted.setId(id);
+//        UserStory saved = userStoryService.update(converted);
+//
+//        return ResponseEntity.ok(saved);
+//    }
+//
+//    @PostMapping("/{us_id}/task")
+//    public ResponseEntity<Void> addTask(@PathVariable("us_id") final Long userStoryId, @RequestBody final UpdateUserStoryDto userStory) {
+//        return null;
+//    }
+//
+//    @DeleteMapping("/{us_id}/task/{task_id}")
+//    public ResponseEntity<Void> deleteTask(@PathVariable("us_id") final Long userStoryId, @PathVariable("task_id") final Long taskId) {
+//        return null;
+//    }
+//
+//    @GetMapping("/{us_id}/tasks")
+//    public ResponseEntity<Iterable<Task>> loadTasks(@PathVariable("us_id") final Long userStoryId) {
+//        return null;
+//    }
+
+    @PutMapping("/{story_id}")
+    @Override
+    public UserStory update(@PathVariable("story_id") final Long userStoryId,
+                            @Valid @RequestBody final UpdateUserStoryDto userStory) {
         return null;
     }
 
-    @GetMapping("/{us_id}")
-    public ResponseEntity findById(@PathVariable("us_id") final Long id) {
-
-        UserStory userStory = userStoryService.findById(id);
-
-        return ResponseEntity.ok(userStory);
-    }
-
-    @PutMapping("/{us_id}")
-    public ResponseEntity<UserStory> update(@PathVariable("us_id") final Long id, @RequestBody final UpdateUserStoryDto userStory) {
-
-        UserStory converted = modelMapper.map(userStory, UserStory.class);
-        converted.setId(id);
-        UserStory saved = userStoryService.update(converted);
-
-        return ResponseEntity.ok(saved);
-    }
-
-    @PostMapping("/{us_id}/task")
-    public ResponseEntity<Void> addTask(@PathVariable("us_id") final Long userStoryId, @RequestBody final UpdateUserStoryDto userStory) {
+    @GetMapping("/{story_id}")
+    @Override
+    public UserStory findById(@PathVariable("story_id") final Long userStoryId) {
         return null;
     }
 
-    @DeleteMapping("/{us_id}/task/{task_id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable("us_id") final Long userStoryId, @PathVariable("task_id") final Long taskId) {
+    @PostMapping("/{story_id}")
+    @Override
+    public Task addTaskToUserStory(@PathVariable("story_id") final Long userStoryId,
+                                   @Valid @RequestBody final CreateTaskDto task) {
         return null;
     }
 
-    @GetMapping("/{us_id}/tasks")
-    public ResponseEntity<Iterable<Task>> loadTasks(@PathVariable("us_id") final Long userStoryId) {
+    @DeleteMapping("/{story_id}/task/{task_id}")
+    @Override
+    public void deleteTaskFromUserStory(@PathVariable("story_id") final Long userStoryId,
+                                        @PathVariable("task_id") final Long taskId) {
+
+    }
+
+    @Override
+    public ResponseEntity<Sprint> loadTasksFromUserStory(Long userStoryId) {
         return null;
     }
+
+
     //endregion
 }
